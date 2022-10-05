8342f5da21572b73d1e2ff6ca58ec34545a91042
RyotaHaya <75255185+watori-studio@users.noreply.github.com>
2022-10-05 14:27:31 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h1>IndexPage</h1>
+		<button v-on:click="movePage1()">page1へ移動($router.push)</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue';
+const router = useRouter();
+const movePage1 = async () => {
+	return navigateTo({
+		path: '/page1',
+		query: {},
+	});
+};
+onBeforeMount(() => {
+	console.log('on before mount');
+});
+
+onMounted(() => {
+	console.log('on mount');
+});
+
+onBeforeUpdate(() => {
+	console.log('on before update');
+});
+
+onUpdated(() => {
+	console.log('on update');
+});
+</script>
---
+++ pages/page1.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<h1>List page</h1>
+		<button v-on:click="moveIndex()">indexへ移動($router.push)</button>
+		<div>search</div>
+		<infinite-loading @infinite="load">
+			<template #spinner> // loading here </template>
+			<template #no-more> // text no more </template>
+			<template #no-results> // text no results </template>
+		</infinite-loading>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from 'vue';
+import InfiniteLoading from 'infinite-loading-vue3-ts';
+const router = useRouter();
+const mounted = ref(false);
+const load = async ($state) => {
+	try {
+		// const data = await fetchLikedTweets('4179070632', nextToken.value);
+		$state.complete();
+	} catch (error) {
+		$state.error();
+	} finally {
+	}
+};
+
+const moveIndex = async () => {
+	return navigateTo({
+		path: '/',
+		query: {},
+	});
+};
+onBeforeMount(() => {
+	console.log('on before mount');
+	mounted.value = true;
+});
+
+onMounted(() => {
+	console.log('on mount');
+});
+
+onBeforeUpdate(() => {
+	console.log('on before update');
+});
+
+onUpdated(() => {
+	console.log('on update');
+});
+</script>
